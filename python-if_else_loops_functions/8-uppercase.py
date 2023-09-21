96bb5bdd92a794e20642710f7a8e2eefa8ad4e6d
AlexM4rcelli <6780@holbertonstudents.com>
2023-09-21 12:44:22 +0000

    Fix some errors

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
-def islower(c):
-    return True if ord(c) >= 97 and ord(c) <= 122 else False
-
-
 def uppercase(input_str):
     str_list = list(input_str)
     for i in range(len(str_list)):
-        if islower(str_list[i]):
+        if ord(str_list[i]) >= 97 and ord(str_list[i]) <= 122:
             up_letter = ord(str_list[i]) + ord('A') - ord('a')
             str_list[i] = chr(up_letter)
     print(''.join(str_list))
-    return ''.join(str_list)
