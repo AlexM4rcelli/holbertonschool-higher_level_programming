dd5b848f038ceb32e754d313c103cb4b59dc4c50
root <6780@holbertonstudents.com>
2023-09-27 15:07:58 +0000

    Fix: print output

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    len_list, errors = 0, 0
+    len_list, printed = 0, 0
     while len_list < x:
         try:
             while len_list < x:
                 print('{:d}'.format(my_list[len_list]), end="")
                 len_list += 1
-            print()
+                printed += 1
         except (ValueError, TypeError):
             len_list += 1
-            errors += 1
             pass
-
-    return len_list - errors
+    print()
+    return printed
