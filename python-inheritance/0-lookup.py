022f01d9f75b1f0691469e339b1d5f811aee15e2
AlexM4rcelli <6780@holbertonstudents.com>
2023-10-04 13:45:26 +0000

    Feat: Task 0 and 1

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Function that returns the list of available attributes and 
+methods of an object
+"""
+
+def lookup(obj):
+    """
+    Args:
+        @obj: object to return their attributes and methods
+    """
+    return list(dir(obj))
---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Defines a class that is a list"""
+class MyList(list):
+    def print_sorted(self):
+        """Prints a sorted list"""
+        if all(isinstance(num, int) for num in self):
+            print(sorted(self))
