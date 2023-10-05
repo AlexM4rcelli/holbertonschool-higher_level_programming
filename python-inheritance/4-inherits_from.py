8f779e9a8deff1b47b90dae3904abb84d049230d
AlexM4rcelli <6780@holbertonschool.com>
2023-10-05 21:11:30 +0000

    Fix some issues

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -4,14 +4,5 @@ from the specified class"""
 
 
 def inherits_from(obj, a_class):
-    """
-        Args:
-            @obj: Object to check if is instance of the other one
-            @a_class: Object to check if is a super class of obj
-        Return:
-            True if the object is exactly an instance of the specified class,
-            otherwise False
-    """
-    if isinstance(obj, a_class) and type(obj) is not a_class:
-        return True
-    return False
+    """check if its a subclass or not"""
+    return isinstance(obj, a_class) and type(obj) is not a_class
