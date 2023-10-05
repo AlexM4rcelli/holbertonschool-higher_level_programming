43eac1055d76b119d1f086eb8f209f35f007e723
AlexM4rcelli <6780@holbertonschool.com>
2023-10-05 21:14:19 +0000

    Fix some issues

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""Check if instance of a class that inherited (directly or indirectly)
-from the specified class"""
+"""Check if instance of a class that inherited (directly or indirectly)"""
 
 
 def inherits_from(obj, a_class):
