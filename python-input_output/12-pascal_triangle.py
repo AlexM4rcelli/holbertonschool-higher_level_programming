c407dd8655498cf22ebce3783138c6b52d519e5f
AlexM4rcelli <6780@holbertonstudents.com>
2023-10-07 23:26:42 +0000

    Feat: Task 12

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -3,6 +3,17 @@
 
 
 def pascal_triangle(n):
-    if n <= 0:
+    """
+    Returns a matrix of integers representing the Pascal's triangle of n
+    """
+    if n <= 0 or not isinstance(n, int):
         return []
-
+    
+    triangle = [[1]]
+    for i in range(1, n):
+        new = [1]
+        for j in range(1, i):
+            new += [triangle[i - 1][j] + triangle[i - 1][j - 1]]
+        new += [1]
+        triangle.append(new)
+    return triangle
