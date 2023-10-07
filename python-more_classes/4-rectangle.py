38a894bcb4df3e83d6f966f76b44671b86e9b8d5
AlexM4rcelli <6780@holbertonstudents.com>
2023-10-07 23:48:04 +0000

    Fix order

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -59,4 +59,4 @@ class Rectangle:
         return result
 
     def __repr__(self):
-        return "Rectangle({},{})".format(self.__width, self.__height)
+        return f"Rectangle({self.__width}, {self.__height})"
