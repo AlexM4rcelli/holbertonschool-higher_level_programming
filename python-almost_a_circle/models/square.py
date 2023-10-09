2ba7ad53a402941b26dab0a086bc4134f63b125f
AlexM4rcelli <6780@holbertonstudents.com>
2023-10-09 17:20:56 +0000

    Fix: pycodestyle

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -3,5 +3,17 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
+    """
+    A class representing a square, which is a special case of a rectangle.
+    """
     def __init__(self, size, x=0, y=0, id=None):
+        """
+        Initialize a Square object.
+
+        Args:
+        - size (int): The size of the square's sides.
+        - x (int, optional): The x-coordinate of the square's position (default is 0).
+        - y (int, optional): The y-coordinate of the square's position (default is 0).
+        - id: An optional identifier for the square.
+        """
         super().__init__(size, size, x, y)
