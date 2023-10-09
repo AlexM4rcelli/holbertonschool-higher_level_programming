a08bf0e9fffa0e79848546127b46d5f0e8fcee5f
AlexM4rcelli <6780@holbertonstudents.com>
2023-10-09 18:20:06 +0000

    Feat: Task 12

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -32,3 +32,17 @@ class Square(Rectangle):
 
     def __str__(self):
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}"
+
+    def update(self, *args, **kwargs):
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.size = args[1]
+            if len(args) >= 3:
+                self.x = args[2]
+            if len(args) == 4:
+                self.y = args[3]
+        else:
+            for name, value in kwargs.items():
+                setattr(self, name, value)
